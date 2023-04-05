036d4afe1bdd3eced4f183ba210eaf4e1206b26e
alexechoi <uceiac8@ucl.ac.uk>
2023-04-05 15:58:00 +0100

    Add TastyAPI

---
+++ TastyAPI/tasty-get.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import os
+import random
+import string
+
+API_KEY = "***REMOVED***"  # Replace this with your actual API key
+BASE_URL = "https://tasty.p.rapidapi.com/recipes/list"
+
+headers = {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "tasty.p.rapidapi.com"
+}
+
+def get_recipes():
+    querystring = {"from": "0", "size": "20", "tags": "under_30_minutes"}
+    response = requests.get(BASE_URL, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error: Unable to fetch recipes. Status code:", response.status_code)
+        return None
+
+def save_to_json_file(data, file_path):
+    file_exists = os.path.isfile(file_path)
+
+    with open(file_path, "a" if file_exists else "w") as f:
+        if file_exists:
+            f.write(",\n")
+        json.dump(data, f, indent=2)
+
+def get_tasty():
+    if __name__ == "__main__":
+    output_dir = "../output"
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dir, "tasty-output.json")
+
+    recipes = get_recipes()
+    
+    if recipes:
+        save_to_json_file(recipes, output_file)
+        print("Recipes saved to:", output_file)
+    else:
+        print("No recipes found.")
