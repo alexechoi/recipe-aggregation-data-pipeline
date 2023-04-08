1b5820d8660fc1a0b092cc7c781cfbc5b7457aed
alexechoi <uceiac8@ucl.ac.uk>
2023-04-08 15:29:00 +0100

    Add MySQL Schema and Test

---
+++ MySQL/schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `recipe` (
+  `id` int PRIMARY KEY,
+  `title` varchar(255),
+  `source_url` varchar(255),
+  `image_url` varchar(255),
+  `category` varchar(255),
+  `cuisine` varchar(255),
+  `instructions` text,
+  `cook_time_minutes` int,
+  `total_time_minutes` int,
+  `yields` varchar(255),
+  `created_at` int,
+  `updated_at` int
+);
+
+CREATE TABLE `ingredient` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `recipe_ingredient` (
+  `recipe_id` int,
+  `ingredient_id` int,
+  `quantity` varchar(255),
+  `unit` varchar(255)
+);
+
+CREATE TABLE `instruction` (
+  `id` int PRIMARY KEY,
+  `recipe_id` int,
+  `position` int,
+  `text` text
+);
+
+CREATE TABLE `nutrition` (
+  `id` int PRIMARY KEY,
+  `recipe_id` int,
+  `calories` int,
+  `protein` int,
+  `fat` int,
+  `carbohydrates` int,
+  `sugar` int,
+  `fiber` int,
+  `cholesterol` int,
+  `sodium` int
+);
+
+ALTER TABLE `recipe_ingredient` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`);
+
+ALTER TABLE `recipe_ingredient` ADD FOREIGN KEY (`ingredient_id`) REFERENCES `ingredient` (`id`);
+
+ALTER TABLE `instruction` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`);
+
+ALTER TABLE `nutrition` ADD FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`);
