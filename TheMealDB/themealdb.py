76615092f792d4536c90eb2d5039e1a6ba6a421d
alexechoi <uceiac8@ucl.ac.uk>
2023-04-09 12:16:18 +0100

    Update TheMealDB JSON format
    Fix function to set JSON format correctly

--- TheMealDB/themealdb.py
+++ TheMealDB/themealdb.py
@@ -16,26 +16,39 @@ def get_random_recipe():
         return None
 
 def save_to_json_file(data, file_path):
-    file_exists = os.path.isfile(file_path)
-    
     meals = data.get("meals", [])
 
-    with open(file_path, "a" if file_exists else "w") as f:
-        if file_exists:
-            f.write(",\n")
-        json.dump(meals, f, indent=2)
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            try:
+                existing_data = json.load(f)
+            except json.decoder.JSONDecodeError:
+                existing_data = []
+    else:
+        existing_data = []
+
+    existing_data.extend(meals)
+
+    with open(file_path, "w") as f:
+        json.dump(existing_data, f, indent=2)
 
 def get_mealdb():
+    output_dir = "../output"
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dir, "themealdb-output.json")
+
+    random_recipe = get_random_recipe()
+
+    if random_recipe:
+        save_to_json_file(random_recipe, output_file)
+        print("Random recipe saved to:", output_file)
+    else:
+        print("No recipe found.")
+
+# FUNCTION TO CALL:
+
+def meal_db_get_reciples(number_of_recipes):
     if __name__ == "__main__":
-        output_dir = "../output"
-        os.makedirs(output_dir, exist_ok=True)
-
-        output_file = os.path.join(output_dir, "themealdb-output.json")
-
-        random_recipe = get_random_recipe()
-        
-        if random_recipe:
-            save_to_json_file(random_recipe, output_file)
-            print("Recipes saved to:", output_file)
-        else:
-            print("No recipes found.")
+        for _ in range(number_of_recipes): # call the function a specified number of times to get multiple recipes
+            get_mealdb()
